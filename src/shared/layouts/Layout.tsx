76282cff9ccd72eb9d637ff2001f8b7ba655b50c
davron129 <normominovdavron001@gmail.com>
2024-02-01 10:18:30 +0500

    feat: layout

--- src/shared/layouts/Layout.tsx
+++ src/shared/layouts/Layout.tsx
@@ -1,5 +1,40 @@
-import { Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
+import { ROUTE_POLYGON_DRAWER, ROUTE_TRACK_MAP, ROUTE_USERS } from "../constants"
 
 export const Layout = () => {
-    return <Outlet />
+    return (
+        <>
+            <header className="px-10 py-5 flex justify-center">
+                <ul className="flex gap-5">
+                    <li>
+                        <Link 
+                            className="font-medium" 
+                            to={ROUTE_TRACK_MAP}
+                        >
+                            Track Map
+                        </Link>
+                    </li>
+                    <li>
+                        <Link 
+                            className="font-medium" 
+                            to={ROUTE_POLYGON_DRAWER}
+                        >
+                            Polygon Drawer
+                        </Link>
+                    </li>
+                    <li>
+                        <Link 
+                            className="font-medium" 
+                            to={ROUTE_USERS}
+                        >
+                            Users
+                        </Link>
+                    </li>
+                </ul>
+            </header>
+            <main>
+                <Outlet />
+            </main>
+        </>
+    )
 }
